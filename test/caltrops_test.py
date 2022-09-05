f8ea8a714e5243075e5a88524a9ecc98da7bb6c7
Jason <jason.a.diamond@gmail.com>
2022-09-04 20:44:17 -0700

    super basic unit tests

---
+++ test/caltrops_test.py
@@ -0,0 +1,26 @@
+import unittest
+import request
+
+CALTROPS_IP = ""
+CALTROPS_PORT = -1
+
+class caltrops_test(unittest.TestCase):
+
+    def test_default_setup(self):
+
+        #test host and port are up and we get the expected iptables rules
+
+        pass
+
+    def test_toggle_judgements(unittest.TestCase):
+        pass
+
+if __name__ == '__main__':
+
+    #target host and port
+
+    #ensure docker container is running
+
+    unittest.main()
+
+    #ensure docker container is shutdown
