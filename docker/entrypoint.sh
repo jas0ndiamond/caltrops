39505071ae5fd4eda42fe5c1383b89ce17a755d5
Jason <jason.a.diamond@gmail.com>
2022-09-04 21:33:22 -0700

    output proxy ports. caltrops in background with sleep inf to block exit

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Launching Caltrops entrypoint"
 
-#launch squid (does not block)
+#launch squid daemon (does not block)
 /usr/sbin/squid -YCd 1 -f /etc/squid/squid.conf
 
 echo "=============="
@@ -10,5 +10,16 @@ echo "=============="
 #buy some time for squid to start up
 sleep 3
 
-#launch caltrops (blocks)
-/usr/bin/python3 /opt/caltrops/caltrops.py
+#launch caltrops
+/usr/bin/python3 /opt/caltrops/caltrops.py &
+
+#buy some time to start up the webserver
+sleep 3
+
+PORT_MIN=3128
+PORT_MAX=3148
+
+#TODO: port range for proxy connections
+echo "Caltrops is listening on ports $PORT_MIN - $PORT_MAX"
+
+sleep infinity
